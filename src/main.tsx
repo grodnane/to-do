1528564b9ee6a03c7febb03c3b9724d231bfb8f5
grodnane <gustavodaniel.rod@gmail.com>
2023-02-01 23:39:33 -0300

    New functionalities and ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ColorModeScript, type ThemeConfig } from '@chakra-ui/react'
+
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark', // 'system' | 'light'
+}
+const theme = extendTheme({config})
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
-  </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
